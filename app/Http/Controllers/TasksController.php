c1ee637b88e4f60ebc58df755262b302328a0d8d
renjiAbarai05 <ritchepadro@gmail.com>
2023-12-11 20:58:48 +0800

    initial commit

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('index', [
+            'tasks' => Task::latest()->paginate(10)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(TaskRequest $request)
+    {
+        $task = Task::create($request->validated());
+    
+        return redirect()->route('tasks.show', ['task' => $task->id])
+            ->with('success', 'Task created succesfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return view('show', [
+            'task' => $task
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
